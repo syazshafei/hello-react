22b760527e6da1dd394722c1ba7a1ea1f9f417de
Mohamad Syazwan Shafei <syazwan567@gmail.com>
2019-09-12 17:43:07 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+class Child extends React.Component {
+    componentDidMount() {
+      const { childRef } = this.props;
+      childRef(this);
+    }
+    componentWillUnmount() {
+     const { childRef } = this.props;
+      childRef(undefined);
+    }
+    alertMessage() {
+      window.alert('called from parent component');
+    }
+    render() {
+      return <h1>Hello World!</h1>
+    }
+  }
+  
+  export default Child;
+
+  class Parent extends React.Component {
+    onClick = () => {
+      this.child.alertMessage(); // do stuff
+    }
+    render() {
+      return (
+        <div>
+          <Child childRef={ref => (this.child = ref)} />
+          <button onClick={this.onClick}>Child.alertMessage()</button>
+        </div>
+      );
+    }
+  }
+
+// ========================================
+
+ReactDOM.render(<Parent />, document.getElementById("root"));

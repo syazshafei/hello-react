0d5a825c3d5fb3490fbcc86582925bd9d63f6528
Mohamad Syazwan Shafei <syazwan567@gmail.com>
2019-09-19 17:40:38 +0800

    Edited

--- src/index.js
+++ src/index.js
@@ -1,50 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class Hello extends React.Component {  
-    render() {  
-        return <h1>Hello world!</h1>;  
-    }  
-}
+// class Hello extends React.Component {  
+//     render() {  
+//         return <h1>Hello world!</h1>;  
+//     }  
+// }
   
-ReactDOM.render(<Hello />, document.getElementById("root"));
+// ReactDOM.render(<Hello />, document.getElementById("root"));
   
 
-// class Child extends React.Component {
-//     componentDidMount() {
-//       const { childRef } = this.props;
-//       childRef(this);
-//     }
-//     componentWillUnmount() {
-//      const { childRef } = this.props;
-//       childRef(undefined);
-//     }
-//     alertMessage() {
-//       window.alert('called from parent component');
-//     }
-//     render() {
-//       return <h1>Hello World!</h1>
-//     }
-//   }
+class Child extends React.Component {
+    componentDidMount() {
+      const { childRef } = this.props;
+      childRef(this);
+    }
+    componentWillUnmount() {
+     const { childRef } = this.props;
+      childRef(undefined);
+    }
+    alertMessage() {
+      window.alert('called from parent component');
+    }
+    render() {
+      return <h1>Hello World!</h1>
+    }
+  }
   
-//   export default Child;
+  export default Child;
 
-//   class Parent extends React.Component {
-//     onClick = () => {
-//       this.child.alertMessage();
-//     }
-//     render() {
-//       return (
-//         <div>
-//           <Child childRef={ref => (this.child = ref)} />
-//           <button onClick={this.onClick}>Child.alertMessage()</button>
-//         </div>
-//       );
-//     }
-//   }
+  class Parent extends React.Component {
+    onClick = () => {
+      this.child.alertMessage();
+    }
+    render() {
+      return (
+        <div>
+          <Child childRef={ref => (this.child = ref)} />
+          <button onClick={this.onClick}>Child.alertMessage()</button>
+        </div>
+      );
+    }
+  }
 
-// // ========================================
+// ========================================
 
-// ReactDOM.render(<Parent />, document.getElementById("root"));
+ReactDOM.render(<Parent />, document.getElementById("root"));
 
 
